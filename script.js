6908a03b82111766c2a7ad4565e4e4e4fb640f67
Alex <Alex3388@live.ca>
2023-08-11 01:22:55 -0700

    Add reset button to clear screen

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 const drawBoard = document.querySelector('.drawboard');
+const resetButton = document.querySelector('#changeResolution');
+
+resetButton.addEventListener('click', function() {
+  drawBoard.innerHTML = ''
+  let resolution = prompt('What resolution?', '16');
+  makeRows(resolution, resolution);
+})
 
 function makeRows(rows, cols) {
     drawBoard.style.setProperty('--grid-rows', rows);
     drawBoard.style.setProperty('--grid-cols', cols);
     for (c = 0; c < (rows * cols); c++) {
       let cell = document.createElement("div");
-      cell.innerText = (c + 1);
       drawBoard.appendChild(cell).className = "grid-item";
     };
   };
   
-  makeRows(16, 16);
 
   document.querySelectorAll('.grid-item').forEach(cell => {
       cell.addEventListener('mouseover',() => {
