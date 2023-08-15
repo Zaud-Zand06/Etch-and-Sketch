b4d25a0183485e2e149a9b25c26ca1cc8c790b35
Alex <Alex3388@live.ca>
2023-08-15 01:50:02 -0700

    Add resolution buttons at top

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
 const drawBoard = document.querySelector('.drawboard');
 const changeResolutionBtn = document.querySelector('#changeResolution');
 const resetBtn = document.querySelector('#reset');
+const sixteenPx = document.getElementById('16x16');
+const thirtytwoPx = document.getElementById('32x32');
+const sixtyfourPx = document.getElementById('64x64');
+const onehundredPx = document.getElementById('128x128');
 
-changeResolutionBtn.addEventListener('click',() => {
-  drawBoard.innerHTML = ''
-  let resolution = prompt('What resolution?', '16'); 
-  parseInt(resolution); //resolution is a interger, valueof returns int
-  if(resolution > 99) {
-    alert('Less than 100 px!')
-    return;
-  } 
-  makeRows(resolution, resolution);
+sixteenPx.addEventListener('click',() => {
+  drawBoard.innerHTML = '';
+  makeRows(16, 16);
+  addTrace();
+});
+
+thirtytwoPx.addEventListener('click',() => {
+  drawBoard.innerHTML = '';
+  makeRows(32, 32);
+  addTrace();
+});
+
+sixtyfourPx.addEventListener('click',() => {
+  drawBoard.innerHTML = '';
+  makeRows(64, 64);
+  addTrace();
+});
+
+onehundredPx.addEventListener('click',() => {
+  drawBoard.innerHTML = '';
+  makeRows(128, 128);
   addTrace();
 });
 
